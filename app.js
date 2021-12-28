15025f7ee35b66920667ae7f7c382b27f93469bb
BeyazGolge <husamcahit@gmail.com>
2021-12-28 19:10:14 +0300

    Added post adding feature

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 const ejs = require("ejs");
+
+//Connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
+//MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.render("index", {
+    posts: posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -21,6 +36,17 @@ app.get("/add_post", (req, res) => {
 
 app.get("/post", (req, res) => {
   res.render("post");
+});
+
+app.post("/posts", async (req, res) => {
+  try {
+    console.log(req.body);
+    await Post.create(req.body);
+    console.log("Post Created");
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
